d2c8db7633787be96ebc9c13a297ac0947be834e
evan <evan.lin.3.141@gmail.com>
2024-11-29 20:40:30 +0000

    Refactor: Reduce duplication code in ColourPaletteTest.
    Refactored ColourPaletteTest to avoid redundant object initialization.

--- src/test/java/test/ColourPaletteTest.java
+++ src/test/java/test/ColourPaletteTest.java
@@ -1,20 +1,27 @@
 package test;
 
 import org.example.ColourPalette;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ColourPaletteTest {
+    private ColourPalette palette;
+    private ColourPalette palette1;
+
+    @BeforeEach
+    void setUp() {
+        palette = new ColourPalette(4);
+        palette1 = new ColourPalette(10);
+    }
     @Test
     void testIfColourPaletteExist() {
-        ColourPalette palette = new ColourPalette();
         assertNotNull(palette, "Colour Palette Instantiated Successfully");
     }
 
     @Test
     void testTotalNumberOfColourInPalette() {
-        ColourPalette palette = new ColourPalette(5);
-        assertEquals(5, palette.getSize());
+        assertEquals(10, palette1.getSize());
     }
 }
