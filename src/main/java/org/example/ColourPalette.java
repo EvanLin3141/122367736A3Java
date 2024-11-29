1078651ab22b7720adbc7c9f090b15fc9fff906e
evan <evan.lin.3.141@gmail.com>
2024-11-29 20:36:34 +0000

    Add: Constructor to take size param for ColourPalette class

--- src/main/java/org/example/ColourPalette.java
+++ src/main/java/org/example/ColourPalette.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public class ColourPalette {
+    private int totalColour;
+
     public ColourPalette() {
-    //pass for now
     }
+
+    public ColourPalette(int size) {
+        this.totalColour = size;
+    }
+
+    public int getSize() {
+        return this.totalColour;
+    }
+
+
 }
--- src/test/java/test/ColourPaletteTest.java
+++ src/test/java/test/ColourPaletteTest.java
@@ -3,11 +3,18 @@ package test;
 import org.example.ColourPalette;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ColourPaletteTest {
     @Test
     void testIfColourPaletteExist() {
         ColourPalette palette = new ColourPalette();
-        assertNotNull(palette);
+        assertNotNull(palette, "Colour Palette Instantiated Successfully");
+    }
+
+    @Test
+    void testTotalNumberOfColourInPalette() {
+        ColourPalette palette = new ColourPalette(5);
+        assertEquals(5, palette.getSize());
     }
 }
